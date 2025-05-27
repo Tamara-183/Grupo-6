ab74dba8c12484e17dccb86b445f976c511bf894
DAVID <DAVID@192.168.0.50>
2025-05-27 10:42:35 -0500

    segunda linea

---
+++ src/main/java/com/mycompany/grupo_6/Grupo_6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupo_6;
+
+/**
+ *
+ * @author DAVID
+ */
+public class Grupo_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
